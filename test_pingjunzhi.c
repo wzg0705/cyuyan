89a753989c3067f37be55627d9dab0d0231c3877
wzg0705 <81730754+wzg0705@users.noreply.github.com>
2021-04-02 19:24:36 +0800

    Add files via upload

---
+++ test_pingjunzhi.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main (void)
+//{
+//    int x,y;
+//    x=3;
+//    y=x*x;
+//    printf("%d = %d * %d\n%d * %d = %d\n",y,x,x,x,x,y);
+//    return 0; 
+//}
+//#include <stdio.h>
+//int main()
+//{
+//    int F=150;
+//     int C=5*(F-32)/9;
+//    printf("Celsius = %d",C);
+//    return 0;
+//}
+#include <stdio.h>
+int main()
+{
+int a=10,t=3,
+float s;
+s=0.5*a*t*t;
+
+ 
+printf("height = %.2",s);
+    return 0;
+}
